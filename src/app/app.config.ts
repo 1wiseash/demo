1feaa62757da791d37318675ba95f91563e174a4
c69219 <ashley.g.williams@hud.gov>
2025-09-29 17:14:01 -0400

    Removed importProvidersFrom as it is no longer needed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,14 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+    // importProvidersFrom(
+    //   provideFirebaseApp(() => initializeApp(environment.firebase)),
+    //   provideFirestore(() => getFirestore()),
+    //   provideAuth(() => getAuth()),
+    //   provideFunctions(() => getFunctions()),
+    //   provideStorage(() => getStorage()),
+    //   provideMessaging(() => getMessaging())
+    // ),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"fir-328c8","appId":"1:96944009075:web:aaf4840b025c539f605f06","storageBucket":"fir-328c8.firebasestorage.app","apiKey":"AIzaSyA1GQ7d3oYRWG4s-pmxe_pdYpTGe_uDml8","authDomain":"fir-328c8.firebaseapp.com","messagingSenderId":"96944009075"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
