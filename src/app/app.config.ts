f55cd59861ba75a541f3ce8819ca9992fe37234f
c69219 <ashley.g.williams@hud.gov>
2025-09-29 18:10:48 -0400

    Post new chats to Firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,7 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
 import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
 import { routes } from './app.routes';
@@ -11,14 +9,6 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // importProvidersFrom(
-    //   provideFirebaseApp(() => initializeApp(environment.firebase)),
-    //   provideFirestore(() => getFirestore()),
-    //   provideAuth(() => getAuth()),
-    //   provideFunctions(() => getFunctions()),
-    //   provideStorage(() => getStorage()),
-    //   provideMessaging(() => getMessaging())
-    // ),
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp({
       "projectId":"fir-328c8",
